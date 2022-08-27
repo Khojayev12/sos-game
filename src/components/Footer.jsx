56b22be2f48d0b0f0b460c8c8f4b543a247384f4
Xo'jayev Yo'ldoshbek <khojayev12@.com>
2022-08-27 12:00:30 +0500

    fsd

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -21,7 +21,7 @@ export default function Footer() {
             <div className="sections">
               <div className="one-section">
                 <a href="/#main" className="my-href">
-                  dasdsa
+                  dasdsa ihbubu
                   {til.main}
                 </a>
               </div>
