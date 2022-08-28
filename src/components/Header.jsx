d80fa869a019540c38a04354b05c8b3247d668fb
Xo'jayev Yo'ldoshbek <khojayev12@.com>
2022-08-28 10:03:00 +0500

    3 sections responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,9 +5,12 @@ import { useState } from "react";
 import steam from "../assets/images/steam.svg";
 import xbox from "../assets/images/xbox.svg";
 import { LangContext, languages } from "./LangContext";
+import { HiMenuAlt3 } from "react-icons/hi";
+import { IoCloseSharp } from "react-icons/io5";
 
 export default function Header(props) {
   const [isChoiceOpen, setIsChoiceOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const handleLang = () => {
     setIsChoiceOpen(!isChoiceOpen);
@@ -16,65 +19,113 @@ export default function Header(props) {
   return (
     <LangContext.Consumer>
       {(til) => (
-        <div className="main-header">
-          <div className="header-center">
-            <div className="main-logo">
-              <img src={logo} alt="" className="logo-pic" />
+        <>
+          <div className="main-header">
+            <div className="header-center">
+              <div className="main-logo">
+                <img src={logo} alt="" className="logo-pic" />
+              </div>
+              <div className="sections">
+                <div className="one-section">
+                  <a href="/#main" className="my-href">
+                    {til.main}
+                  </a>
+                </div>
+                <div className="one-section">
+                  <a href="/#about" className="my-href">
+                    {til.about}
+                  </a>
+                </div>
+                <div className="one-section">
+                  <a href="/#features" className="my-href">
+                    {til.gameFeatures}
+                  </a>
+                </div>
+                <div className="one-section">
+                  <a href="/#requirements" className="my-href">
+                    {til.systemRequirements}
+                  </a>
+                </div>
+                <div className="one-section">
+                  <a href="/#quotes" className="my-href">
+                    {til.quotes}
+                  </a>
+                </div>
+              </div>
+              <div className="settings">
+                <div className="language" onClick={handleLang}>
+                  {til.til}
+                  <VscChevronDown
+                    className={isChoiceOpen ? "up-arrow" : "down-arrow"}
+                  />
+                </div>
+                <div className="social">
+                  <img src={xbox} alt="" className="social-pic" />
+                  <img src={steam} alt="" className="social-pic" />
+                </div>
+                {isChoiceOpen ? (
+                  <div className="selectoin">
+                    <div
+                      className="choice"
+                      onClick={() => {
+                        props.setTil(languages.uz);
+                        setIsChoiceOpen(false);
+                      }}
+                    >
+                      UZB
+                    </div>
+                    <div
+                      className="choice"
+                      onClick={() => {
+                        props.setTil(languages.en);
+                        setIsChoiceOpen(false);
+                      }}
+                    >
+                      ENG
+                    </div>
+                  </div>
+                ) : (
+                  <></>
+                )}
+              </div>
             </div>
-            <div className="sections">
-              <div className="one-section">
-                <a href="/#main" className="my-href">
-                  {til.main}
-                </a>
+          </div>
+          <div className="mobile-menu">
+            <div className="mobile-header">
+              <div className="mobile-logo">
+                <img src={logo} alt="" className="mobile-logo-pic" />
               </div>
-              <div className="one-section">
-                <a href="/#about" className="my-href">
-                  {til.about}
-                </a>
-              </div>
-              <div className="one-section">
-                <a href="/#features" className="my-href">
-                  {til.gameFeatures}
-                </a>
-              </div>
-              <div className="one-section">
-                <a href="/#requirements" className="my-href">
-                  {til.systemRequirements}
-                </a>
-              </div>
-              <div className="one-section">
-                <a href="/#quotes" className="my-href">
-                  {til.quotes}
-                </a>
-              </div>
-            </div>
-            <div className="settings">
-              <div className="language" onClick={handleLang}>
-                {til.til}
-                <VscChevronDown
-                  className={isChoiceOpen ? "up-arrow" : "down-arrow"}
-                />
-              </div>
-              <div className="social">
-                <img src={xbox} alt="" className="social-pic" />
-                <img src={steam} alt="" className="social-pic" />
+
+              <div className="mobile-change-lang">
+                <div
+                  className="mob-change-lang-text"
+                  onClick={() => {
+                    setIsChoiceOpen(!isChoiceOpen);
+                  }}
+                >
+                  {til.til}
+                  <VscChevronDown
+                    className={isChoiceOpen ? "up-arrow" : "down-arrow"}
+                  />
+                </div>
               </div>
               {isChoiceOpen ? (
-                <div className="selectoin">
+                <div className="mobile-lang-choice">
                   <div
-                    className="choice"
+                    className="mob-lang-choice-one"
+                    style={{ marginTop: "5px" }}
                     onClick={() => {
                       props.setTil(languages.uz);
-                      setIsChoiceOpen(false);
+                      setIsChoiceOpen(false)
                     }}
                   >
                     UZB
                   </div>
                   <div
-                    className="choice"
+                    className="mob-lang-choice-one"
                     onClick={() => {
                       props.setTil(languages.en);
-                      setIsChoiceOpen(false);
+                      setIsChoiceOpen(false)
                     }}
                   >
                     ENG
@@ -83,9 +134,82 @@ export default function Header(props) {
               ) : (
                 <></>
               )}
+
+              <div
+                className="mob-hamburger"
+                onClick={() => {
+                  setIsMenuOpen(true);
+                }}
+              >
+                <HiMenuAlt3 />
+              </div>
+            </div>
+            <div
+              className={
+                isMenuOpen
+                  ? "mob-menu-div menu-active"
+                  : "mob-menu-div menu-de-active"
+              }
+            >
+              <IoCloseSharp
+                onClick={() => {
+                  setIsMenuOpen(false);
+                }}
+                className="menu-close-btn"
+              />
+              <button
+                className="menu-sections-each"
+                onClick={() => {
+                  setIsMenuOpen(false);
+                }}
+              >
+                <a href="./#main" className="my-href">
+                  {til.main}
+                </a>
+              </button>
+              <button
+                className="menu-sections-each"
+                onClick={() => {
+                  setIsMenuOpen(false);
+                }}
+              >
+                <a href="./#about" className="my-href">
+                  {til.about}
+                </a>
+              </button>
+              <button
+                className="menu-sections-each"
+                onClick={() => {
+                  setIsMenuOpen(false);
+                }}
+              >
+                <a href="./#features" className="my-href">
+                  {til.gameFeatures}
+                </a>
+              </button>
+              <button
+                className="menu-sections-each"
+                onClick={() => {
+                  setIsMenuOpen(false);
+                }}
+              >
+                <a href="./#requirements" className="my-href">
+                  {til.systemRequirements}
+                </a>
+              </button>
+              <button
+                className="menu-sections-each"
+                onClick={() => {
+                  setIsMenuOpen(false);
+                }}
+              >
+                <a href="./#quotes" className="my-href">
+                  {til.quotes}
+                </a>
+              </button>
             </div>
           </div>
-        </div>
+        </>
       )}
     </LangContext.Consumer>
   );
