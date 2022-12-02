f2154c76c40b7143615fd99e7ed5b76f030ace47
Xo'jayev Yo'ldoshbek <khojayev12@.com>
2022-12-02 12:44:09 +0500

    123

--- src/components/Carusel.jsx
+++ src/components/Carusel.jsx
@@ -21,16 +21,22 @@ export default function Carusel() {
         src={pic1}
         alt=""
         className={first === 1 ? "all-pic  front" : first === 2 ? "all-pic middle" : "all-pic last"}
+        width={first === 1 ? "501px" : first === 2 ? "483px" : "483px"}
+        height={first === 1 ? "610px" : first === 2 ? "563px" : "511px"}
       />
       <img
         src={pic2}
         alt=""
         className={first === 1 ? "all-pic middle" : first === 2 ? "all-pic last" : "all-pic front"}
+        width={first === 1 ? "483px" : first === 2 ? "483px" : "501px"}
+        height={first === 1 ? "563px" : first === 2 ? "511px" : "610px"}
       />
       <img
         src={pic3}
         alt=""
         className={first === 1 ? "all-pic last" : first === 2 ? "all-pic front" : " all-pic middle"}
+        width={first === 1 ? "483px" : first === 2 ? "501px" : "483px"}
+        height={first === 1 ? "511px" : first === 2 ? "610px" : "563px"}
       />
 
       <BsCaretDown className="slide-icon" onClick={slider} />
